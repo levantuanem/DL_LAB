9f8de0f62c910bb22d93f2c9c5fb728c3fd02c3a
Le Van Tuan Em <levantuanem00@gmail.com>
2026-09-01 21:38:16 +0700

    Learning Cv2

---
+++ src/Computer_vision.py
@@ -0,0 +1,9 @@
+import cv2
+image = cv2.imread(r'D:\AI_Projects\DL_LAB\data\raw\images\effen.jpg') #BGR
+#Lấy ra từng kênh màu 1 
+image = image[:,:,1] = 0 # blue
+image = image[:,:,2] = 0
+print(image.shape) # chiều dọc-> ngang -> sâu
+cv2.imshow('image', image)
+cv2.waitKey(0)
+# cv2.destroyAllWindows()
